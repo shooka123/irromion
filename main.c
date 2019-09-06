5197c90ff91aacb3512c71c0da206cb60eeae4bf
shooka <shooka@localhost.localdomain>
2019-09-06 12:44:45 -0400

    Upload

--- main.c
+++ main.c
@@ -1,14 +1,33 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <libdill.h>
 
-static coroutine void attacker();
+#define VICTIM_IPV4 "www.google.com"
+#define VICTIM_PORT 443
+
+static coroutine void attacker(const struct ipaddr *remote_addr);
 
 int main(void) {
-    go(attacker());
+    struct ipaddr remote_addr;
+    if (ipaddr_remote(&remote_addr, VICTIM_IPV4, VICTIM_PORT, 0, now() + 7000) == -1) {
+        perror("Failed to recognise the victim's IP");
+        return EXIT_FAILURE;
+    }
+
+    go(attacker(&remote_addr));
     return EXIT_SUCCESS;
 }
 
-static coroutine void attacker() {
+static coroutine void attacker(const struct ipaddr *remote_addr) {
+    int tcp_stream;
+    try_connect:;
+    if ((tcp_stream = tcp_connect(remote_addr, now() + 7000)) == -1) {
+        perror("Failed to connect a TCP stream");
+        goto try_connect;
+    }
 
+    if (tcp_close(tcp_stream, now() + 7000) == -1) {
+        perror("Failed to close a TCP stream");
+    }
 }
